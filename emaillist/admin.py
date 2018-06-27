6236f81cd11143bfa156d288e03dffa5dfd4d0f9
james <illuillu55@hotmail.com>
2018-06-27 15:52:29 +0900

    Initial commit

---
+++ emaillist/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from emaillist.models import Emaillist
+
+# Register your models here.
+
+admin.site.register(Emaillist)
---
+++ emaillist/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Emaillist(models.Model):  # DB에 있는 이메일 리스트를 보여준다. CRUD 중에 CR만 있다.
+    first_name = models.CharField(max_length=50)   # varchar에 max length 값 주기.
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):  # 자바의 toString() 함수와 비슷하다.
+        return 'Emaillist(%s, %s, %s)' % (self.first_name, self.last_name, self.email)
+
